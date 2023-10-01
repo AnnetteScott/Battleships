c324a5428a64844ded718a0ae97c379cacfc879c
AnnetteScott <notnatural21@gmail.com>
2023-10-01 18:17:48 +1300

    Begin scaffolding the team class

--- src/battleships/Team.java
+++ src/battleships/Team.java
@@ -5,5 +5,36 @@ package battleships;
  * @author gmt3870
  */
 public abstract class Team {
+    private final Board board;
+    private final String name;
+    private final int score;
 
+    public Team(String name, int score){
+        this.name = name;
+        this.score = score;
+        this.board = new Board();
+    }
+    
+    /**
+     * @return the board
+     */
+    public Board getBoard() {
+        return board;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the score
+     */
+    public int getScore() {
+        return score;
+    }
+    
+    
 }
