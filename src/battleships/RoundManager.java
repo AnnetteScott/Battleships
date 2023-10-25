ddc4cb6022f6ea6f513bb772e712596e74748413
AnnetteScott <notnatural21@gmail.com>
2023-10-26 12:38:57 +1300

    Allow rounds to be restarted

--- src/battleships/RoundManager.java
+++ src/battleships/RoundManager.java
@@ -5,5 +5,31 @@ package battleships;
  * @author gmt3870
  */
 public class RoundManager {
+    private Board playerBoard;
+    private Board enemyBoard;
+
+    public RoundManager(){
+        this.playerBoard = new Board();
+        this.enemyBoard = new Board();
+    }
+
+    /**
+     * @return the playerBoard
+     */
+    public Board getPlayerBoard() {
+        return this.playerBoard;
+    }
+
+    /**
+     * @return the enemyBoard
+     */
+    public Board getEnemyBoard() {
+        return this.enemyBoard;
+    }
+
+    public void restartRound(){
+        this.playerBoard.initialiseBoard();
+        this.enemyBoard.initialiseBoard();
+    }
 
 }
