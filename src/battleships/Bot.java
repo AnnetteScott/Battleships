fa1dec8e996c0747052cfd48c86612d90d7f4790
AnnetteScott <notnatural21@gmail.com>
2023-10-26 11:18:46 +1300

    Add constructor methods

--- src/battleships/Bot.java
+++ src/battleships/Bot.java
@@ -5,5 +5,7 @@ package battleships;
  * @author gmt3870
  */
 public class Bot extends Team{
-
+    public Bot(){
+        super("Bot", 0);
+    }
 }
--- src/battleships/Player.java
+++ src/battleships/Player.java
@@ -5,5 +5,7 @@ package battleships;
  * @author gmt3870
  */
 public class Player extends Team {
-
+    public Player(String name, int score){
+        super("Player", score);
+    }
 }
