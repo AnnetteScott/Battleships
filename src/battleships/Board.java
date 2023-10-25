2eff419bdcfeedc84875701b2c08b8c21a290670
AnnetteScott <notnatural21@gmail.com>
2023-10-26 11:19:15 +1300

    Initialise board with ships

--- src/battleships/Board.java
+++ src/battleships/Board.java
@@ -1,9 +1,35 @@
 package battleships;
 
+import battleships.Ships.*;
+
 /**
  *
  * @author gmt3870
  */
 public class Board {
-
+    private Ship[] fleet;
+    private Point[][] grid;
+    private final int SHIP_AMOUNT = 5;
+    private final int BOARD_LENGTH = 10;
+    
+    public Board(){
+        this.fleet = new Ship[SHIP_AMOUNT];
+        this.fleet[0] = new Carrier();
+        this.fleet[1] = new Battleship();
+        this.fleet[2] = new Destroyer();
+        this.fleet[3] = new Submarine();
+        this.fleet[4] = new Patrol();
+        
+        this.grid = new Point[BOARD_LENGTH][BOARD_LENGTH];
+        
+        for (int x = 0; x < BOARD_LENGTH; x++) {
+            for (int y = 0; y < BOARD_LENGTH; y++) {
+                grid[x][y] = new Point(x, y);
+            }
+        }
+        
+        ShipPlacer placer = new ShipPlacer();
+        
+        placer.placeShips(grid, fleet);
+    }
 }
