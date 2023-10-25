253403bdd1524d89f9d83c9a4ac7efd0753b5f0e
AnnetteScott <notnatural21@gmail.com>
2023-10-26 12:41:07 +1300

    Create and display the gui

--- src/battleships/Battleships.java
+++ src/battleships/Battleships.java
@@ -1,5 +1,9 @@
 package battleships;
 
+import javax.swing.*;
+import java.awt.*;
+import battleships.UI.*;
+
 /**
  *
  * @author gmt3870
@@ -10,7 +14,31 @@ public class Battleships {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        JFrame frame = new JFrame();
+        frame.setResizable(false);
+        frame.setTitle("Battleships");
+        frame.setSize(1200, 700);
+        frame.setLocation(1500, 100);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		RoundManager roundMng = new RoundManager();
+
+        MainMenu mainMenu = new MainMenu();
+        GamePage gamePage = new GamePage(roundMng);
+        ScorePage scorePage = new ScorePage();
+
+        CardLayout cardLayout = new CardLayout();
+        JPanel mainPanel = new JPanel(cardLayout);
+        mainPanel.add(mainMenu, "Menu");
+        mainPanel.add(gamePage, "Game");
+        mainPanel.add(scorePage, "Scores");
+
+        cardLayout.show(mainPanel, "Menu");
+
+        frame.add(mainPanel);
+
+        frame.setVisible(true);
+
     }
     
 }
