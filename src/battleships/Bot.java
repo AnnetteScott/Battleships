f87665a61487bce65c9adc29f33851ecdd356cc6
AnnetteScott <notnatural21@gmail.com>
2023-10-26 21:35:24 +1300

    Allow the bot to take a turn

--- src/battleships/Bot.java
+++ src/battleships/Bot.java
@@ -1,11 +1,29 @@
 package battleships;
 
+import java.util.Random;
+
 /**
  *
  * @author gmt3870
  */
 public class Bot extends Team{
-    public Bot(){
-        super("Bot", 0);
+    
+    private final Random random;
+    
+    public Bot(int score){
+        super("Bot", score);
+        this.random = new Random();
+    }
+    
+    public void takeTurn(Board board){
+        boolean valid = false;
+        int x;
+        int y;
+        
+        while(!valid){
+            x = random.nextInt(board.getBOARD_LENGTH());
+            y = random.nextInt(board.getBOARD_LENGTH());
+            valid = board.fireShot(x, y);
+        }
     }
 }
