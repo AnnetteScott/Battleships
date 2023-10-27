917d0e907449fbdc6e360a73826bd203953914f0
AnnetteScott <notnatural21@gmail.com>
2023-10-27 16:11:56 +1300

    Display scores on the main menu

--- src/battleships/Battleships.java
+++ src/battleships/Battleships.java
@@ -14,16 +14,18 @@ public class Battleships {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        DBManager dbMng = new DBManager();
+        RoundManager roundMng = new RoundManager(dbMng);
+        
         JFrame frame = new JFrame();
         frame.setResizable(false);
         frame.setTitle("Battleships");
         frame.setSize(1200, 700);
         frame.setLocation(1500, 100);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		RoundManager roundMng = new RoundManager();
-
-        MainMenu mainMenu = new MainMenu();
+        
+        
+        MainMenu mainMenu = new MainMenu(roundMng);
         GamePage gamePage = new GamePage(roundMng);
 
         CardLayout cardLayout = new CardLayout();
